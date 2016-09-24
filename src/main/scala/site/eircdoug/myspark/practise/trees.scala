f9efaad52fb53e93b6460c2a24dbc86b84b0230d
EricDoug <eric_doug@126.com>
2016-09-24 17:28:19 +0800

    add practise

---
+++ src/main/scala/site/eircdoug/myspark/practise/trees.scala
@@ -0,0 +1,21 @@
+package site.eircdoug.myspark.practise
+
+import org.apache.spark.SparkContext
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.rdd.RDD
+
+/**
+  * Created by ericdoug on 16-9-24.
+  */
+object trees {
+
+  def data_handler(sc:SparkContext, data_file:String) : RDD[LabeledPoint] = {
+
+  }
+
+  def main(args: Array[String]) {
+
+
+  }
+
+}
